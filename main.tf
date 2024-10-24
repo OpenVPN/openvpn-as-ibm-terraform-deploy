b70e60227cb4452af4ca1e56061d2fd2c56ddaf5
joao-diasx <joao.regino@cogni-ext.com>
2024-10-24 12:04:53 -0300

    Fixing code

--- main.tf
+++ main.tf
@@ -1,16 +1,1 @@
-module "vpc" {
-  source = "./vpc.tf"
-}
 
-module "instance" {
-  source = "./instance.tf"
-}
-
-module "image" {
-  source = "./image.tf"
-}
-
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  region           = var.region
-}
--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "openvpn_public_ip" {
-  description = "The public IP address of the OpenVPN server"
-  value       = ibm_is_instance.openvpn_instance.primary_network_interface.0.floating_ip
+  value = ibm_is_instance.openvpn_instance.primary_network_interface.0.primary_ipv4_address
 }
+
 
 output "openvpn_instance_id" {
   description = "The instance ID of the OpenVPN server"
