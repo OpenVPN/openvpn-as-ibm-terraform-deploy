3bd8cfa14dea9be510a3a381bee386e80cd33009
joao-diasx <joao.regino@cogni-ext.com>
2025-02-11 10:12:31 -0300

    Update OpenVPN installation version and improve variable descriptions in cloud-config and vars.tf

--- vars.tf
+++ vars.tf
@@ -1,17 +1,17 @@
 variable "dns_create" {
   type        = bool
-  description = "Create DNS entries (true/false)"
+  description = "Set to true to create DNS entries, or false to skip creating them."
   default     = false
 }
 
 variable "dns_domain_name" {
   type        = string
-  description = "Name of the DNS domain (example.com)"
+  description = "DNS domain name (e.g., example.com)."
 }
 
 variable "dns_entry_name" {
   type        = string
-  description = "DNS entry name (www)"
+  description = "DNS entry name (e.g., www)."
 }
 
 variable "dns_responsible_person" {
@@ -22,18 +22,18 @@ variable "dns_responsible_person" {
 
 variable "ibmcloud_api_key" {
   type        = string
-  description = "IBM Cloud API key"
+  description = "IBM Cloud API key for authentication."
 }
 
 variable "instance_name" {
   type        = string
-  description = "Name of the OpenVPN instance"
+  description = "OpenVPN instance name."
   default     = "openvpn-as-server"
 }
 
 variable "instance_profile" {
   default     = "cx2-2x4"
-  description = "VM size and family"
+  description = "VM size and family profile (e.g., cx2-2x4)."
 }
 
 variable "image_name" {
@@ -44,64 +44,58 @@ variable "image_name" {
 
 variable "region" {
   type        = string
-  description = "IBM Cloud region"
+  description = "IBM Cloud region name."
   default     = "us-east"
 }
 
 variable "region_zone" {
   type        = string
-  description = "IBM Cloud zone region"
+  description = "Availability zone name within the specified region."
   default     = "us-east-1"
 }
 
 variable "ssh_public_key" {
   default     = ""
-  description = "Public ssh ID name. This needs to be pre-created."
+  description = "Pre-created SSH public key name for instance access."
 }
 
 variable "vpc_cidr_block" {
   type        = string
-  description = "CIDR block for the subnet"
+  description = "The CIDR block for the VPC subnet (e.g., 192.168.0.0/16)."
 }
 
 variable "vpc_create" {
   type        = bool
-  description = "Set to true to create a new VPC, false to use an existing VPC"
+  description = "Set to true to create a new VPC, or false to use an existing VPC."
   default     = false
 }
 
 variable "vpc_existing_name" {
   type        = string
-  description = "Name of the existing VPC to use when vpc_create is false"
+  description = "The name of the existing VPC to use when vpc_create is set to false."
   default     = ""
 }
 
 variable "vpc_name" {
   type        = string
-  description = "Name of the VPC"
+  description = "The name to assign to the new VPC."
   default     = "openvpn-vpc"
 }
 
 variable "vpc_public_gateway_name" {
   type        = string
-  description = "Name of the public gateway"
+  description = "The name of the public gateway to attach to the VPC."
   default     = "openvpn-pgw"
 }
 
 variable "vpc_security_group_name" {
   type        = string
-  description = "Name of the security group"
+  description = "The name of the security group for the VPC."
   default     = "openvpn-sg"
 }
 
 variable "vpc_subnet_name" {
   type        = string
-  description = "Name of the Subnet"
+  description = "The name of the subnet in the VPC."
   default     = "openvpn-subnet"
-}
-
-variable "openvpn_version" {
-  description = "Version of OpenVPN to install"
-  type        = string
-  default     = "2.14.1"
 }
